d78e66d4b057abc20ab7e420b1d35532c0b87aa4
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-02-06 21:52:32 +1000

    Ship class is complete

---
+++ board.py
@@ -0,0 +1,13 @@
+class Board:
+    """This class describes game-board for both players human and AI"""
+    def __init__(self):
+        self.board = None
+        self.ships = None
+        self.hid = False
+        self.alive_ships = None
+
+    def add_ship(self):
+        pass
+
+    def contour(self):
+        pass
--- ship.py
+++ ship.py
@@ -9,9 +9,13 @@ class Ship:
 
     def __init__(self):
         self.length = self.course = None
-        #self.bow = Dot()
+        # self.bow = Dot()
         self._dots = []
         self.hit_points = None
+        self.name = None
+
+    def get_name(self):
+        return self.name
 
     @property
     def dots(self):
@@ -19,16 +23,33 @@ class Ship:
 
     @dots.setter
     def dots(self, data):
-        x, y, self.course, self.length = (_ for _ in data)
-        #self.bow.coords = x, y
-        self.hit_points = self.length
+        x, y, self.course = (_ for _ in data)
         if self.course:
             self._dots = [Dot(x, y) for x in range(x, x + self.length)]
+        else:
+            self._dots = [Dot(x, y) for y in range(y, y + self.length)]
 
+
+class Boat(Ship):
+    def __init__(self):
+        self.length = self.hit_points = 1
+        self.name = 'boat'
+
+
+class Destroyer(Ship):
+    def __init__(self):
+        self.length = self.hit_points = 2
+        self.name = 'destroyer'
+
+
+class Cruiser(Ship):
+    def __init__(self):
+        self.length = self.hit_points = 3
+        self.name = 'cruiser'
 
 
 if __name__ == '__main__':
-    ship = Ship()
-    ship.dots = 1, 4, 1, 3
+    ship = Cruiser()
+    ship.dots = 1, 2, 1
     for dot in ship.dots:
         print(dot.coords)
